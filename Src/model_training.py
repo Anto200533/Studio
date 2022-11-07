5d94d04c0ab53a6b9a8310d10a311dea9b5dd050
Anto200533 <antojohnmathews@gmail.com>
2022-11-07 15:15:04 +1100

    final changes

--- Src/model_training.py
+++ Src/model_training.py
@@ -5,13 +5,13 @@
 #
 # Author : Anto
 # ***********************************************************************************************************
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.optimizers import RMSprop
 import numpy as np
-import os
-import pickle
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, LSTM, Dropout
-from string import punctuation
+import random
+import sys
+
 
 # ***********************************************************************************************************
 #
@@ -19,85 +19,79 @@ from string import punctuation
 #
 # ***********************************************************************************************************
 
-sequence_length = 100
-BATCH_SIZE = 128
-EPOCHS = 30
-
 # ***********************************************************************************************************
 #
 #  fill in later
 #
 # ***********************************************************************************************************
 
-
-def get_path(path):
+def get_path():
     try:
         path = 'Src/scripts.txt'
-        basename = os.path.basename(path)
         text = open(path, encoding="utf-8").read()
         text = text.lower()
         return text
     except Exception as e:
         raise Exception(str(e))
 
+
+
+
 # ***********************************************************************************************************
 #
 #  fill in later
 #
 # ***********************************************************************************************************
 
-def data_stats(text):
-    try:
-        n_char = len(text)
-        unique_char = ''.join(sorted(set(text)))
-        no_of_characters = ("No of Characater:", n_char)
-        no_of_uniq_characters = ("No Of Unique Characters:", unique_char)
-        return no_of_characters, no_of_uniq_characters
-    except Exception as e:
-        raise Exception(str(e))
+file = get_path()
 
-test = 'Src/scripts.txt'
-a = get_path(test)
-b = data_stats(a)
-print(b)
+chars = sorted(list(set(file)))
 
-# no_of_characters = ("No of Characater:", n_char)
-# no_of_uniq_characters = ("No Of Unique Characters:", n_unique_char)
+char_to_int = dict((c,i) for i,c in enumerate(chars))
 
+int_to_char = dict((i,c) for i,c in enumerate(chars))
 
-# char2int = {c: i for i, c in enumerate(unique_char)}
-# int2char = {i: c for i, c in enumerate(unique_char)}
-# pickle.dump(char2int, open(f"{'script'}-char2int.pickle", "wb"))
-# pickle.dump(int2char, open(f"{'script'}-int2char.pickle", "wb"))
+n_chars = len(file)
+n_vocab = len(chars)
+
+seq_length = 60  
+step = 10   
+sentences = []   
+next_chars = []  
+for i in range(0, n_chars - seq_length, step):  
+    sentences.append(file[i: i + seq_length])  
+    next_chars.append(file[i + seq_length])  
+n_patterns = len(sentences)    
 
 
-# encoded_text = np.array([char2int[c] for c in get_path()])
-# char_dataset = tf.data.Dataset.from_tensor_slices(encoded_text)
-# sequences = char_dataset.batch(
-#     2*sequence_length + 1, drop_remainder=True)
+x = np.zeros((len(sentences), seq_length, n_vocab), dtype=np.bool_)
+y = np.zeros((len(sentences), n_vocab), dtype=np.bool_)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_to_int[char]] = 1
+    y[i, char_to_int[next_chars[i]]] = 1
 
-# # for sequence in sequences.take(2):
-# #     print(''.join([int2char[i] for i in sequence.numpy()]))
+model = Sequential()
+model.add(LSTM(128, input_shape=(seq_length, n_vocab)))
+model.add(Dense(n_vocab, activation='softmax'))
 
+optimizer = RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.summary()
 
-# def split_sample(sample):
-#     ds = tf.data.Dataset.from_tensors(
-#         (sample[:sequence_length], sample[sequence_length]))
-#     for i in range(1, (len(sample)-1) // 2):
-#         input_ = sample[i: i+sequence_length]
-#         target = sample[i+sequence_length]
-#         other_ds = tf.data.Dataset.from_tensors((input_, target))
-#         ds = ds.concatenate(other_ds)
-#     return ds
+from keras.callbacks import ModelCheckpoint
 
+filepath="saved_weights/saved_weights-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 
-# dataset = sequences.flat_map(split_sample)
-
-
-# def one_hot_samples(input_, target):
-#     return tf.one_hot(input_, n_unique_char), tf.one_hot(target, n_unique_char)
+callbacks_list = [checkpoint]
 
 
-# dataset = dataset.map(one_hot_samples)
 
-# ds = dataset.repeat().shuffle(1024).batch(BATCH_SIZE, drop_remainder=True)
+
+history = model.fit(x, y,
+          batch_size=128,
+          epochs=50,   
+          callbacks=callbacks_list)
+
+model.save('my_saved_weights_jungle_book_50epochs.h5')
